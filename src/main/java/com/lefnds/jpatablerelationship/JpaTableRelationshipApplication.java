745613ab15de36820c6e94a52195468f71e4ba16
Edu <luizeduardofnds@gmail.com>
2022-12-11 14:41:10 -0300

    code structure

---
+++ src/main/java/com/lefnds/jpatablerelationship/JpaTableRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.lefnds.jpatablerelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTableRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTableRelationshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lefnds/jpatablerelationship/JpaTableRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lefnds.jpatablerelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTableRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
